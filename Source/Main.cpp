7e3aa8e75a0443dcf8ae315f93709beec3048f55
Index <64417343+TDSIndex@users.noreply.github.com>
2024-11-30 10:22:47 +1000

    Removed Example Print

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -5,8 +5,6 @@
 #include <Windows.h>
 
 #include <Include.hpp>
-#include <print>
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-    std::println(TEST_STRING);
 }
