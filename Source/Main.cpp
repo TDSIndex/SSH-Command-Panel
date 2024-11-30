13c68cf6c35326450d77e9ebc4f2dda54c181eb8
Index <64417343+TDSIndex@users.noreply.github.com>
2024-11-30 10:21:46 +1000

    Changed To Windows Entry Point

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,12 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <Windows.h>
+
 #include <Include.hpp>
 #include <print>
 
-int main(int argc, char *argv[]) {
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
     std::println(TEST_STRING);
 }
